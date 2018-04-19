d6a0c6f2f3e46549adf757a91ff93b6c8c9b162c
bell <bell.sph@gmail.com>
2018-04-19 20:03:40 +0700

    delete bash file

--- started.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker/docker-compose.yml build --pull web_blog
-
-docker-compose -f docker/docker-compose.yml run --rm web_blog pip install -r requirements.txt
-
-docker-compose -f docker/docker-compose.yml run --rm web_blog python manage.py migrate
-
-docker-compose -f docker/docker-compose.yml up --build
