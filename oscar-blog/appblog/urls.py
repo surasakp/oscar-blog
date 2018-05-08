543104427afb32ff26095c6ea35447af00f9fce9
Hardwire <admin@admins-MacBook-Pro.local>
2018-05-08 07:55:38 +0700

    remove comment line

--- oscar-blog/appblog/urls.py
+++ oscar-blog/appblog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-# from .views import index
+
 from appblog.dashboard.apps import application as blog_dashboard_application
+
 
 urlpatterns = [
     url(r'^post/', include(blog_dashboard_application.urls))
